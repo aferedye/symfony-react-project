8a7f0924d4702ca9b3f31f5d867535a7b9285638
Feredye Antoine <aferedye@gmail.com>
2020-06-10 16:28:00 +0200

    New components Boutique

---
+++ assets/js/components/Boutique.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Boutique extends Component {
+    render() {
+        return (
+            <h1>Bienvenue dans notre Boutique</h1>
+        )
+    }
+}
+
+export default Boutique;
