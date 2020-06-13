2a3d1d70fecd97079be3e77f463842f0fa122785
Feredye Antoine <aferedye@gmail.com>
2020-06-13 10:32:47 +0200

    Update Boutique.js

--- assets/js/components/Boutique.js
+++ assets/js/components/Boutique.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class Boutique extends Component {
+  
     render() {
         return (
-            <h1>Bienvenue dans notre Boutique</h1>
+            <div className="App" ref={el => (this.div = el)}>
+                <h1>Bienvenue dans notre Boutique</h1>
+            </div>
         )
     }
 }
